7396190e05638ec357dc558cb57bfdae82774398
Helder <helderlucau@gmail.com>
2023-09-04 02:02:17 +0300

    Completed assignment

--- lib/seed.py
+++ lib/seed.py
@@ -14,3 +14,20 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+
+# Clear our database before each new seed 
+session.query(Game).delete()
+session.commit()
+
+# Faker to generate 50 random games
+games = [
+    Game(
+        title=fake.name(),
+        genre=fake.word(),
+        platform=fake.word(),
+        price=random.randint(0, 60) # Use random library to generate prices
+    )
+for i in range(50)]
+
+session.bulk_save_objects(games)
+session.commit()
